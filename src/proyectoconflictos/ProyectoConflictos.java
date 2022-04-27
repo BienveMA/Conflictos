e4c56b40219800a1c8fdf73984617af2eb5813cf
Bienve <morenoalvarezb@iesmartinrivero.org>
2022-04-27 14:43:50 +0200

    Tercer Commit, modificado proyectoConflictos

--- src/proyectoconflictos/ProyectoConflictos.java
+++ src/proyectoconflictos/ProyectoConflictos.java
@@ -17,6 +17,7 @@ public class ProyectoConflictos {
     public static void main(String[] args) {
         // TODO code application logic here
         System.out.println("Creado por Bienve");
+        System.out.println("Modificado por Bienve");
     }
     
 }
