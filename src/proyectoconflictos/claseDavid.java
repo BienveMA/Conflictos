19f93d29e2286c667b90c8013b6e2ecee0b97127
David Guerrero <guerreromunozd@iesmartinrivero.org>
2022-04-27 14:32:18 +0200

    Añadida claseDavid

---
+++ src/proyectoconflictos/claseDavid.java
@@ -0,0 +1,15 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package proyectoconflictos;
+
+/**
+ *
+ * @author Usuario
+ */
+public class claseDavid {
+    
+    String creado = "Clase creada por David";
+}
