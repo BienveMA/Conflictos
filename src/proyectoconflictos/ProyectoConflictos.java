c630e5ea34e1727fb0e52ccd572743ce1a22d922
Bienve <morenoalvarezb@iesmartinrivero.org>
2022-04-27 14:08:52 +0200

    Segundo Commit, añadida ClaseBienve

---
+++ src/proyectoconflictos/ProyectoConflictos.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package proyectoconflictos;
+
+/**
+ *
+ * @author usuario
+ */
+public class ProyectoConflictos {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        System.out.println("Creado por Bienve");
+    }
+    
+}
